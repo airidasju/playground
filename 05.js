a47e3baf480149942e51a6719de03dbc089e7e34
Airidas Juska <airidas665@gmail.com>
2022-12-15 19:40:36 +0200

    05

---
+++ 05.js
@@ -0,0 +1,24 @@
+function prevMultOfThree (n) {
+
+    if(n % 3 == 0) {
+      return n
+    }
+
+    nArray = n.toString().split('');
+    let arrLength = nArray.length
+    for(let i = 0; i < arrLength; i++) {
+        num = nArray.splice(-1);
+        nNum = nArray.join("")
+        if(nNum % 3 == 0) {
+          nNum = Number(nNum)
+          if(nNum == 0) {
+            return null
+            }
+          return nNum   
+        }
+      }
+    }
+
+
+
+  prevMultOfThree( 25)
