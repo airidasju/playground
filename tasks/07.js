5037a6740f47f5244b1a7772a6db3cd8ef204c89
Airidas Juska <airidas665@gmail.com>
2022-12-19 16:09:27 +0200

    07 done

--- tasks/07.js
+++ tasks/07.js
@@ -1,14 +1,17 @@
 function getCount(str) {
-  let counter = 0
+    let counter = 0
   strSplit = str.split("")
   for(let i = 0; i < strSplit.length; i++) {
-    switch(i) {
+    switch(strSplit[i]) {
     case "a":
     case "e":
     case "i":
     case "o":
-    case "u": counter += 1
+    case "u": counter++
+    break;
     }
   }
   return counter
 }
+
+getCount("abracadabra")
