994765b5ecfbb4d485f668067171e6682376ca3e
Airidas Juska <airidas665@gmail.com>
2022-12-19 15:21:40 +0200

    06 done

--- tasks/06.js
+++ tasks/06.js
@@ -1,63 +1,72 @@
-function hasSurvived(attackers, defenders){
-    let initialAttackerPower = 0
-    for (let i = 0; i < attackers.length; i ++) {
-        initialAttackerPower += attackers[i];
+function timeCorrect(timestring) {
+    let minuteCounter;
+    let secondCounter;
+
+
+    if(typeof timestring !== "string") {
+        return null
+    } else if (timestring == "") {
+        return ""
     }
-    console.log(initialAttackerPower)
 
-    let initialDefenderPower = 0
-    for (let i = 0; i < defenders.length; i++) {
-        initialDefenderPower += defenders[i];
+    let strArr = timestring.split(":")
+
+    for(let i = 0; i < strArr.length; i++ ) {
+        if(strArr[i].match("[a-zA-Z]+")) {
+        return null}
     }
-    console.log(initialDefenderPower)
 
-    let popped;
-
-    let attackerPower = initialAttackerPower;
-    let defenderPower = initialDefenderPower
-    if(attackers.length == defenders.length) {
-        for(let i = 0; i <= attackers.length; i++) {
-            // if(attackers[i] == undefined) {
-            //     attackers[i] = 0
-            // }
-
-            // if(defenders[i] == undefined) {
-            //     defenders[i] = 0
-            // }
-
-            if(attackers[i] > defenders[i]) {
-            attackerPower -= defenders[i]
-            attackers[i] -= defenders[i]
-            popped = defenders[i]
-            console.log(attackers, defenders)
-            } else if (defenders[i] > attackers[i]) {
-            defenderPower -= attackers[i]
-            defenders[i] -= attackers[i]
-            popped = attackers[i]
-            console.log(attackers, defenders)
-           } else {
-            attackerPower -= defenders[i]
-            defenderPower -= attackers[i]
-            popped = defenders[i]
-            popped = attackers[i]
-            console.log(attackers, defenders)
-           }
+        if(strArr.length == 1) {
+            return null
         } 
-    }
+        
+        if (strArr[2] >= 60) {
+            for(let i = strArr[2]; i >= 0; i -= 60) {
 
-        if(attackers.length < defenders.length){
-            return true;
-        } else if (attackers.length > defenders.length) {
-            return false;
-        } else {
-            if(initialAttackerPower < initialDefenderPower) {
-                return true
-            } else {
-                return false
+                strArr[2] = i
+                secondCounter = 1
+            }
+            strArr[1] = strArr[1] * 1
+            strArr[1] = strArr[1] + 1
+            if(strArr[2] < 10) {
+                strArr[2] = strArr[2].toString();
+                strArr[2] = "0" + strArr[2]
+            }
+        }   
+        
+        if(strArr[1] >= 60) {
+            for(let i = strArr[1]; i >= 0; i -= 60) {
+                strArr[1] = i
+                minuteCounter = 1
+            }
+            strArr[0] = strArr[0] * 1
+            strArr[0] = strArr[0] + 1
+            if(strArr[1] < 10) {
+                strArr[1] = strArr[1].toString();
+                strArr[1] = "0" + strArr[1]
             }
         }
-        
-  }
+
+        if(strArr[0] >= 24) {
+            for(let i = strArr[0]; i >= 0; i -= 24) {
+                strArr[0] = i
+                
+            }
+            
+            if(strArr[0] < 10) {
+                strArr[0] = strArr[0].toString();
+                strArr[0] = "0" + strArr[0]
+            }
+
+        }
+        console.log(strArr)
+        let correctedStr = strArr.join(":")
+        console.log(correctedStr);
+    return correctedStr;
+}
+  
+timeCorrect("11:70:10")
+timeCorrect("19:99:99")
+timeCorrect("24:01:01")
 
 
-  hasSurvived([2, 9, 9, 7], [1, 1, 3, 8])
