efa272a4ac01225fee42387f9884396193fe8364
Airidas Juska <airidas665@gmail.com>
2022-12-20 11:45:26 +0200

    task 10

--- tasks/10.js
+++ tasks/10.js
@@ -1,21 +1,20 @@
 function highAndLow(numbers){
-    let lowest = numbers[0];
-    let highest =  numbers[0];
     let numArr = String(numbers).split(" ").map((numbers)=>{
         return Number(numbers)
-      })
+    })
+    let lowest = numArr[0];
+    let highest =  numArr[0];
       for(let i = 1; i < numArr.length; i++) {
         if(numArr[i] < lowest) {
             lowest = numArr[i]
-            console.log("asd")
         }
         if(numArr[i] > highest) {
             highest = numArr[i]
-            console.log(highest)
         }
     }
+
     let answer = String(highest) + " " + String(lowest)
     return answer
   }
 
-  highAndLow("-24 -577 -982 -613 110 -1030 505 -287 -953 -1671 642 -1765 -487 -939 -756 1211 1281 -1068 879 -909 962")
+  highAndLow("-3481 -3786 -1583 -1408 -2896 -4176 -1521 -2746 -1080 -2393 -1496 -2565 -2291 -3507 -1957 -3716 -2636 -4152 -3004 -3224 -2991")
