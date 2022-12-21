91e0ef3deb7a96eb08e0ebc679aa89706c0ca06f
Airidas Juska <airidas665@gmail.com>
2022-12-21 17:03:02 +0200

    13 unfinished

--- tasks/13.js
+++ tasks/13.js
@@ -1,5 +1,25 @@
 // 6 kyu
 
 function wave(str){
-    // Code here
-  }
+    const result = []
+    let upperArray = []
+    let upperLetter;
+    let strArr = str.split('')
+    let length = strArr.length
+    for(let i=0; i < length; i++) {
+        upperArray.push(strArr)
+        // console.log(upperArray)
+    }
+    
+    for(let i=0; i < length; i++) {
+        if(upperArray[i][i] == upperArray[i][i].toLowerCase()) {
+            letter = upperArray[i][i].toUpperCase()
+            result.push(letter, [...upperArray[i][i].toLowerCase()])
+        }
+        // result.push(upperArray[i][i].toUpperCase())
+    }
+    console.log(result)
+}
+
+
+  wave("codewars")
