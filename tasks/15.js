6b8db075feee453da571902c47a3435feae907b1
Airidas Juska <airidas665@gmail.com>
2022-12-23 14:19:12 +0200

    15 done

--- tasks/15.js
+++ tasks/15.js
@@ -1,1 +1,27 @@
 // 6 kyu
+
+function findOdd(A) {
+    const sortedArr = A.sort((a, b) => a - b)
+    let counter = 1;
+    for(let i = 0; i <sortedArr.length; i++) {
+
+    if( i !== 0 && sortedArr[i] == sortedArr[i - 1] ){
+        counter++
+        }
+
+        
+        if(sortedArr[i] !== sortedArr[i + 1]){
+            if(counter % 2 !== 0) {
+            return sortedArr[i]
+                    }
+             counter = 0
+             counter++
+        }
+    }
+    return 0;
+  }
+
+//   if(sortedArr[i] !== sortedArr[i + 1]) {
+//     counter = 0
+// }
+  findOdd([-2,-2,-1,-1,1,1,2,2,3,3,4,4,5,5,5,20,20])
