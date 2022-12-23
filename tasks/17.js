beb007f95a3984a2250c3931e522e4a90325ed14
Airidas Juska <airidas665@gmail.com>
2022-12-23 16:30:48 +0200

    17 not done

--- tasks/17.js
+++ tasks/17.js
@@ -1,2 +1,24 @@
 // 6 kyu
 
+function digitalRoot(n) {
+    let sum = 0;
+    let numArr = String(n).split("").map((n)=>{
+        return Number(n)
+      })
+      for(let i=0; i < numArr.length; i++) {
+        sum += numArr[i]
+       
+    }
+    console.log(sum)
+     if(sum < 10) {
+            console.log("answer", sum)
+            return sum
+        } else if (sum > 10) {
+            console.log("recursive",sum)
+            digitalRoot(sum)
+        }
+   
+}
+
+//   console.log(digitalRoot(16))
+ console.log(digitalRoot(493193))
