7a9d3c230f8262b21dc5d61dece2304ef3ed651c
Airidas Juska <airidas665@gmail.com>
2022-12-24 13:42:46 +0200

    17 done

--- tasks/17.js
+++ tasks/17.js
@@ -1,20 +1,21 @@
 // 6 kyu
 
 function digitalRoot(n) {
-    let sum = 0;
-    let numArr = String(n).split("").map((n)=>{
-        return Number(n)
-      })
-      console.log(numArr)
-      for(let i=0; i < numArr.length; i++) {
-        sum += numArr[i]
-    }
-     if(sum < 10) {
-            return sum
-        } else if (sum >= 10) {
-            return digitalRoot(sum)
-        }
-   
+  let sum = 0;
+  let numArr = String(n)
+    .split('')
+    .map((n) => {
+      return Number(n);
+    });
+  console.log(numArr);
+  for (let i = 0; i < numArr.length; i++) {
+    sum += numArr[i];
+  }
+  if (sum < 10) {
+    return sum;
+  } else if (sum >= 10) {
+    return digitalRoot(sum);
+  }
 }
 
- console.log(digitalRoot(649206))
+console.log(digitalRoot(649206));
