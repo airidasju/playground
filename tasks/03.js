0ac13c9dd9e933a0f5020e86fbdfc97caea4d924
Airidas Juska <airidas665@gmail.com>
2022-12-25 19:48:53 +0200

    cleanup

--- tasks/03.js
+++ tasks/03.js
@@ -1,8 +1,7 @@
 const summation = function (num) {
-    let number = 0
-    for(let i = 0; i <= num; i++)
-    number += i
-    return number;
-}
+  let number = 0;
+  for (let i = 0; i <= num; i++) number += i;
+  return number;
+};
 
-  summation(8)
+summation(8);
