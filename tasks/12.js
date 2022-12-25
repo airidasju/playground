fe7a0e2e77609b2eef2e5aea7dd8e4d52418b15f
Airidas Juska <airidas665@gmail.com>
2022-12-25 19:47:52 +0200

    cleanup

--- tasks/12.js
+++ tasks/12.js
@@ -1,23 +1,21 @@
 // 6 kyu
 
-function solution(number){
-    let num = Number(number)
-    let numArr = []
-    let dividiblesArr = []
-    let sum = 0;
-    for(let i = 1; i < num; i++) {
-        numArr.push(i)
-    }
+function solution(number) {
+  let num = Number(number);
+  let numArr = [];
+  let dividiblesArr = [];
+  let sum = 0;
+  for (let i = 1; i < num; i++) {
+    numArr.push(i);
+  }
 
-    for(let i = 0; i < numArr.length; i++) {
-        if (numArr[i] % 3 == 0  || numArr[i] % 5 == 0) {
-            dividiblesArr.push(numArr[i])
-            sum += numArr[i]
-        }
+  for (let i = 0; i < numArr.length; i++) {
+    if (numArr[i] % 3 == 0 || numArr[i] % 5 == 0) {
+      dividiblesArr.push(numArr[i]);
+      sum += numArr[i];
     }
-    return sum;
+  }
+  return sum;
 }
 
-
-
-solution(10)
+solution(10);
