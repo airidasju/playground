11ba51adda4ff00741ee0915668d076315224540
Airidas Juska <airidas665@gmail.com>
2022-12-25 19:48:22 +0200

    cleanup

--- tasks/08.js
+++ tasks/08.js
@@ -1,5 +1,5 @@
 function disemvowel(str) {
-    const reg1 = /[aeiou]/gi
-    let newStr =  str.replace(reg1, "")
-    return newStr
-  }
+  const reg1 = /[aeiou]/gi;
+  let newStr = str.replace(reg1, '');
+  return newStr;
+}
