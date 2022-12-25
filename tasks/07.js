f244c2eca310a390ad70912d124433f7267a9c35
Airidas Juska <airidas665@gmail.com>
2022-12-25 19:48:30 +0200

    cleanup

--- tasks/07.js
+++ tasks/07.js
@@ -1,17 +1,18 @@
 function getCount(str) {
-    let counter = 0
-  strSplit = str.split("")
-  for(let i = 0; i < strSplit.length; i++) {
-    switch(strSplit[i]) {
-    case "a":
-    case "e":
-    case "i":
-    case "o":
-    case "u": counter++
-    break;
+  let counter = 0;
+  strSplit = str.split('');
+  for (let i = 0; i < strSplit.length; i++) {
+    switch (strSplit[i]) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+        counter++;
+        break;
     }
   }
-  return counter
+  return counter;
 }
 
-getCount("abracadabra")
+getCount('abracadabra');
