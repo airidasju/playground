a2abad3b2e9aa2c0e1c664fa8e0791ff87ea8de7
Airidas Juska <airidas665@gmail.com>
2022-12-25 19:48:12 +0200

    cleanup

--- tasks/09.js
+++ tasks/09.js
@@ -1,13 +1,14 @@
-function squareDigits(num){
-    numArr = String(num).split("").map((num)=>{
-        return Number(num)
-      })
-    for(let i = 0; i < numArr.length; i++) {
-        numArr[i]= numArr[i] * numArr[i]
-    }
-    numArr = numArr.join("")
-    return Number(numArr)
+function squareDigits(num) {
+  numArr = String(num)
+    .split('')
+    .map((num) => {
+      return Number(num);
+    });
+  for (let i = 0; i < numArr.length; i++) {
+    numArr[i] = numArr[i] * numArr[i];
   }
+  numArr = numArr.join('');
+  return Number(numArr);
+}
 
-
-  squareDigits(2112)
+squareDigits(2112);
