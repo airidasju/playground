b8079d349f1f1f075e1089c4617507cc756b5ee5
Airidas Juska <airidas665@gmail.com>
2022-12-25 19:47:10 +0200

    cleanup

--- tasks/16.js
+++ tasks/16.js
@@ -1,17 +1,16 @@
 // 8 kyu
 
-const hotpo = function(n){
-    let counter = 0;
-    while (n !== 1) {
-        if(n % 2 == 0) {
-            n = n/2
-        } else if (n % 2 !== 0 ) {
-            n = 3 * n + 1
-        }
-    counter++
-}
-return counter; 
-    
+const hotpo = function (n) {
+  let counter = 0;
+  while (n !== 1) {
+    if (n % 2 == 0) {
+      n = n / 2;
+    } else if (n % 2 !== 0) {
+      n = 3 * n + 1;
+    }
+    counter++;
   }
+  return counter;
+};
 
-  hotpo(23)
+hotpo(23);
