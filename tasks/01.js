3152edb09581af98a8a05b9db7cabaf98a675aed
Airidas Juska <airidas665@gmail.com>
2022-12-25 19:49:21 +0200

    cleanup

--- tasks/01.js
+++ tasks/01.js
@@ -1,18 +1,18 @@
 function cupAndBalls(b, arr) {
   let bigArr = arr;
-  let smallestArr = bigArr;
 
   for (let z = 0; z < bigArr.length; z++) {
-
-      if(bigArr[z][1] == b) {
-        b = bigArr[z][0]
-      } else if (bigArr[z][0] == b) {
-          b = bigArr[z][1]
+    if (bigArr[z][1] == b) {
+      b = bigArr[z][0];
+    } else if (bigArr[z][0] == b) {
+      b = bigArr[z][1];
     }
   }
-  console.log(b)
 }
 
-// cupAndBalls(2, [[1, 2]]);
-// cupAndBalls(1,[[2,3],[1,2],[1,2]])
-cupAndBalls(2,[[1,3],[1,2],[2,1],[2,3]])
+cupAndBalls(2, [
+  [1, 3],
+  [1, 2],
+  [2, 1],
+  [2, 3],
+]);
