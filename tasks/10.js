542b234c9deec40287cc15f550550d1c3b6a170f
Airidas Juska <airidas665@gmail.com>
2022-12-25 19:48:06 +0200

    cleanup

--- tasks/10.js
+++ tasks/10.js
@@ -1,20 +1,24 @@
-function highAndLow(numbers){
-    let numArr = String(numbers).split(" ").map((numbers)=>{
-        return Number(numbers)
-    })
-    let lowest = numArr[0];
-    let highest =  numArr[0];
-      for(let i = 1; i < numArr.length; i++) {
-        if(numArr[i] < lowest) {
-            lowest = numArr[i]
-        }
-        if(numArr[i] > highest) {
-            highest = numArr[i]
-        }
+function highAndLow(numbers) {
+  let numArr = String(numbers)
+    .split(' ')
+    .map((numbers) => {
+      return Number(numbers);
+    });
+  let lowest = numArr[0];
+  let highest = numArr[0];
+  for (let i = 1; i < numArr.length; i++) {
+    if (numArr[i] < lowest) {
+      lowest = numArr[i];
     }
-
-    let answer = String(highest) + " " + String(lowest)
-    return answer
+    if (numArr[i] > highest) {
+      highest = numArr[i];
+    }
   }
 
-  highAndLow("-3481 -3786 -1583 -1408 -2896 -4176 -1521 -2746 -1080 -2393 -1496 -2565 -2291 -3507 -1957 -3716 -2636 -4152 -3004 -3224 -2991")
+  let answer = String(highest) + ' ' + String(lowest);
+  return answer;
+}
+
+highAndLow(
+  '-3481 -3786 -1583 -1408 -2896 -4176 -1521 -2746 -1080 -2393 -1496 -2565 -2291 -3507 -1957 -3716 -2636 -4152 -3004 -3224 -2991',
+);
