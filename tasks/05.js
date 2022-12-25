19cfb092223bc69134fcf1bbd2ea08291ec25bfa
Airidas Juska <airidas665@gmail.com>
2022-12-25 19:48:43 +0200

    cleanup

--- tasks/05.js
+++ tasks/05.js
@@ -1,24 +1,21 @@
-function prevMultOfThree (n) {
+function prevMultOfThree(n) {
+  if (n % 3 == 0) {
+    return n;
+  }
 
-    if(n % 3 == 0) {
-      return n
-    }
-
-    nArray = n.toString().split('');
-    let arrLength = nArray.length
-    for(let i = 0; i < arrLength; i++) {
-        num = nArray.splice(-1);
-        nNum = nArray.join("")
-        if(nNum % 3 == 0) {
-          nNum = Number(nNum)
-          if(nNum == 0) {
-            return null
-            }
-          return nNum   
-        }
+  nArray = n.toString().split('');
+  let arrLength = nArray.length;
+  for (let i = 0; i < arrLength; i++) {
+    num = nArray.splice(-1);
+    nNum = nArray.join('');
+    if (nNum % 3 == 0) {
+      nNum = Number(nNum);
+      if (nNum == 0) {
+        return null;
       }
+      return nNum;
     }
+  }
+}
 
-
-
-  prevMultOfThree( 25)
+prevMultOfThree(25);
