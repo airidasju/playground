4e24275bc5c6f7f22f6703597a8e85bcf7fcdf2e
Airidas Juska <airidas665@gmail.com>
2022-12-25 19:47:36 +0200

    cleanup

--- tasks/14.js
+++ tasks/14.js
@@ -1,17 +1,16 @@
 // 6 kyu
 
-function spinWords(string){
-    const resultArr = []
-    const stringArr = string.split(" ")
-    for(let i = 0; i< stringArr.length; i++) {
-        if(stringArr[i].length >= 5) {
-            resultArr.push(stringArr[i].split("").reverse().join(""));
-        } else {
-            resultArr.push(stringArr[i])
-        }
+function spinWords(string) {
+  const resultArr = [];
+  const stringArr = string.split(' ');
+  for (let i = 0; i < stringArr.length; i++) {
+    if (stringArr[i].length >= 5) {
+      resultArr.push(stringArr[i].split('').reverse().join(''));
+    } else {
+      resultArr.push(stringArr[i]);
     }
-    return resultArr.join(" ")
   }
+  return resultArr.join(' ');
+}
 
-
-  spinWords("Hey fellow warriors")
+spinWords('Hey fellow warriors');
