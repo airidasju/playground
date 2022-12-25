89b69a89eef269fd6ef688530cb25f1d4191e862
Airidas Juska <airidas665@gmail.com>
2022-12-25 19:47:45 +0200

    cleanup

--- tasks/13.js
+++ tasks/13.js
@@ -1,19 +1,19 @@
 // 6 kyu
 
 function wave(str) {
-    let result = [];
-  
-    for (let i = 0; i < str.length; i++) {
-      if (str[i] !== ' ') {
-        let newStr = str.substring(0, i) + str[i].toUpperCase() + str.substring(i + 1);
-        result.push(newStr);
-      }
+  let result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      let newStr =
+        str.substring(0, i) + str[i].toUpperCase() + str.substring(i + 1);
+      result.push(newStr);
     }
-    console.log(result)
-    return result;
   }
+  console.log(result);
+  return result;
+}
 
+wave('two words');
 
-  wave("two words")
-
-  //'Two words', 'tWo words', 'twO words', 'two Words', 'two wOrds', 'two woRds', 'two worDs', 'two wordS'
+//'Two words', 'tWo words', 'twO words', 'two Words', 'two wOrds', 'two woRds', 'two worDs', 'two wordS'
