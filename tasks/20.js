02a12ac9a75d2e0073905b213ecadf4807ec7f32
Airidas Juska <airidas665@gmail.com>
2023-01-03 17:06:11 +0200

    unfinished

---
+++ tasks/20.js
@@ -0,0 +1,29 @@
+function arrayDiff(a, b) {
+  if(a[0] == undefined || b[0] == undefined) {
+    console.log("empty")
+    return
+  }
+
+
+
+function search(value) {
+    for (let i=0; i < b.length; i++) {
+        if(value !== b[i]) {
+            // console.log(value)
+            return true
+        }
+        else {
+            console.log(value)
+            return false
+        }
+    }
+    // return returnedValue
+}
+
+const filtered = a.filter(search)
+console.log(filtered)
+}
+
+
+
+arrayDiff([1,2,3], [1,2])
