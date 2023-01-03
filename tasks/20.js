a7abe694e5dabf43b8ebe7be60a8c7e885b6ca73
Airidas Juska <airidas665@gmail.com>
2023-01-03 17:18:59 +0200

    20 finished

--- tasks/20.js
+++ tasks/20.js
@@ -1,27 +1,13 @@
 function arrayDiff(a, b) {
-  if(a[0] == undefined || b[0] == undefined) {
+  if(b[0] == undefined) {
     console.log("empty")
-    return
+    return a
   }
 
 
 
-function search(value) {
-    for (let i=0; i < b.length; i++) {
-        if(value !== b[i]) {
-            // console.log(value)
-            return true
-        }
-        else {
-            console.log(value)
-            return false
-        }
-    }
-    // return returnedValue
-}
-
-const filtered = a.filter(search)
-console.log(filtered)
+const filtered = a.filter(val => !b.includes(val));
+return filtered
 }
 
 
