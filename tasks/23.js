0fbc182ca819eb97122365af9089b292614d95c5
Airidas Juska <airidas665@gmail.com>
2023-01-04 23:47:06 +0200

    23 done

---
+++ tasks/23.js
@@ -0,0 +1,16 @@
+// 6 kyu
+
+
+function duplicateCount(text){
+    const strArr = text.toLowerCase().split('').sort()
+    let count = 0
+    for(let i = 0; i < strArr.length;  i++) {
+        if (strArr[i] == strArr[i + 1] && strArr[i] != strArr[i + 2]) {
+            count++
+        }
+    }
+    return count
+}
+
+
+  duplicateCount("Indivisibilities")
