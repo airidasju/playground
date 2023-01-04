a3d3378889cb1ce2422ce8221565ae4a0a1df7e2
Airidas Juska <airidas665@gmail.com>
2023-01-05 00:17:47 +0200

    24 done

--- tasks/24.js
+++ tasks/24.js
@@ -1,9 +1,41 @@
 // 6 kyu
 
+function isValidWalk(walk) {
+  let n = 0;
+  let s = 0;
+  let w = 0;
+  let e = 0;
 
-function duplicateEncode(word){
-    // ...
+  if (walk.length != 10) {
+    console.log('false');
+    return false;
+  }
+
+  for (let i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case 'n':
+        n++;
+        break;
+      case 's':
+        s++;
+        break;
+      case 'w':
+        w++;
+        break;
+      case 'e':
+        e++;
+        break;
+    }
+  }
+
+  if (s == n && w == e) {
+    console.log('true');
+    return true;
+  } else {
+    console.log('false');
+    return false;
+  }
 }
 
-
-duplicateEncode("Success")
+isValidWalk(['n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's']);
+isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']);
