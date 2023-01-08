db895a6fa351e233b421a116fa25f859a539cc1f
Airidas Juska <airidas665@gmail.com>
2023-01-08 23:33:15 +0200

    upd

--- tasks/23.js
+++ tasks/23.js
@@ -1,16 +1,14 @@
 // 6 kyu
 
-
-function duplicateCount(text){
-    const strArr = text.toLowerCase().split('').sort()
-    let count = 0
-    for(let i = 0; i < strArr.length;  i++) {
-        if (strArr[i] == strArr[i + 1] && strArr[i] != strArr[i + 2]) {
-            count++
-        }
+function duplicateCount(text) {
+  const strArr = text.toLowerCase().split('').sort();
+  let count = 0;
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] == strArr[i + 1] && strArr[i] != strArr[i + 2]) {
+      count++;
     }
-    return count
+  }
+  return count;
 }
 
-
-  duplicateCount("Indivisibilities")
+duplicateCount('Indivisibilities');
